4004638cab398aa6ed69796e881d8990e243e621
Harry-Just-ZJU <hanyang.24@intl.zju.edu.cn>
2025-05-12 16:36:05 +0800

    20250512

--- 2.2/2.2.py
+++ 2.2/2.2.py
@@ -1,18 +1,24 @@
 
+# 处理数据
 from Bio.Seq import Seq
 my_seq = Seq("AGTACACTGGT")
-print(my_seq)               # AGTACACTGGT
+print(my_seq)   
 
-print(my_seq.complement())  # TCATGTGACCA
+print(my_seq.complement()) 
 
-print(my_seq.reverse_complement())   # ACCAGTGTACT
+print(my_seq.reverse_complement())   
 
+
+# 解析文件
 from Bio import SeqIO
+
+# FASTA
 for seq_record in SeqIO.parse('ls_orchid.fasta', 'fasta'):
     print(seq_record.id)
     print(repr(seq_record.seq))
     print(len(seq_record))
 
+# GenBank
 for seq_record in SeqIO.parse("ls_orchid.gbk", "genbank"):
     print(seq_record.id)
     print(repr(seq_record.seq))
--- 2.3/2.3.py
+++ 2.3/2.3.py
@@ -1,17 +1,22 @@
+
+# 处理成‘字符串’
 from Bio.Seq import Seq
 my_seq = Seq("GATCG")
 for index, letter in enumerate(my_seq):
     print("%i %s" % (index, letter))
 
+# 计数
 from Bio.Seq import Seq
 print("AAAA".count("AA"))
 print(Seq("AAAA").count("AA"))
 
+# 算GC含量
 from Bio.Seq import Seq
 from Bio.SeqUtils import gc_fraction
 my_seq= Seq('GATCGATGGGCCTATATAGGATCGAAAATCGC')
 print(gc_fraction(my_seq))
 
+# 切片
 from Bio.Seq import Seq
 my_seq= Seq("GATCGATGGGCCTATATAGGATCGAAAATCGC")
 print(my_seq[4:12])
@@ -20,13 +25,15 @@ print(my_seq[1::3])
 print(my_seq[2::3])
 print(my_seq[::-1])
 
+# fasta格式
 fasta_format_string = ">Name\n%s\n" % my_seq
 print(fasta_format_string)
 
+# 连接序列
 from Bio.Seq import Seq
 protein_seq= Seq("EVRNAK")
 dna_seq =Seq("ACGT")
-print(protein_seq+ dna_seq)
+print(protein_seq + dna_seq)
 
 from Bio.Seq import Seq
 list_of_seqs =[Seq("ACGT"), Seq("AACC"), Seq("GGTT")]
@@ -40,10 +47,58 @@ contigs =[Seq("ATG"),Seq("ATCCCG"),Seq("TTGCA")]
 spacer = Seq("N"*10)
 print(spacer.join(contigs))
 
+# 改大小写
 from Bio.Seq import Seq
 dna_seq =Seq("acgtACGT")
 print(dna_seq.upper())
 
 print("GTAC" in dna_seq)
 print("GTAC" in dna_seq.upper())
+
+# 转录
+from Bio.Seq import Seq
+coding_dna = Seq("ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG")
+messenger_rna = coding_dna.transcribe()
+print(messenger_rna)
+print(messenger_rna.back_transcribe())
+
+# 翻译
+from Bio.Seq import Seq
+messenger_rna = Seq("AUGGCCAUUGUAAUGGGCCGCUGAAAGGGUGCCCGAUAG")
+print(messenger_rna.translate())
+coding_dna = Seq("ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG")
+print(coding_dna.translate())
+
+print(coding_dna.translate(table="Vertebrate Mitochondrial"))
+print(coding_dna.translate(table=2))
+print(coding_dna.translate(to_stop=True))
+print(coding_dna.translate(table=2, to_stop=True))
+print(coding_dna.translate(table=2, stop_symbol="@"))
+
+from Bio.Seq import Seq
+gene = Seq("GTGAAAAAGATGCAATCTATCGTACTCGCACTTTCCCTGGTTCTGGTCGCTCCCATGGCA" + \
+           "GCACAGGCTGCGGAAATTACGTTAGTCCCGTCAGTAAAATTACAGATAGGCGATCGTGAT" + \
+           "AATCGTGGCTATTACTGGGATGGAGGTCACTGGCGCGACCACGGCTGGTGGAAACAACAT" + \
+           "TATGAATGGCGAGGCAATCGCTGGCACCTACACGGACCGCCGCCACCGCCGCGCCACCAT" + \
+           "AAGAAAGCTCCTCATGATCATCACGGCGGTCATGGTCCAGGCAAACATCACCGCTAA")
+
+print(gene.translate(table="Bacterial"))
+print(gene.translate(table="Bacterial", to_stop=True))
+print(gene.translate(table="Bacterial", cds=True))
+
+# 翻译表
+from Bio.Data import CodonTable
+
+standard_table= CodonTable.unambiguous_dna_by_name["Standard"]
+mito_table= CodonTable.unambiguous_dna_by_name["Vertebrate Mitochondrial"]
+
+standard_table= CodonTable.unambiguous_dna_by_id[1]
+mito_table= CodonTable.unambiguous_dna_by_id[2]
+
+print(standard_table)
+print(mito_table)
+
+print(mito_table.stop_codons)
+print(mito_table.start_codons)
+print(mito_table.forward_table["ACG"])
 
